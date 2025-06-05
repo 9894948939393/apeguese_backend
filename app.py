f0dde3dd59d1a94ba14c85c96bfc88f253e0850d
Ana Laura <analauracnunes@icloud.com>
2025-06-05 14:29:22 -0300

    Update app.py

--- app.py
+++ app.py
@@ -482,9 +482,9 @@ def criar_app():
         origin = request.headers.get("Origin")
         if origin in origins:
             response.headers["Access-Control-Allow-Origin"] = origin
-        response.headers["Access-Control-Allow-Credentials"] = "true"
-        response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
-        response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS,PUT,DELETE"
-        return response
+            response.headers["Access-Control-Allow-Credentials"] = "true"
+            response.headers["Access-Control-Allow-Headers"] = "Content-Type,Authorization"
+            response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS,PUT,DELETE"
+            return response
         
     return app
